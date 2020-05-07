1e69a61c73be4e698720010e760debd967547d31
karina-grinkevich <grinkevich.karina@gmail.com>
2020-05-07 22:05:00 +0300

    1.10

--- frida/js/fullpage.js
+++ frida/js/fullpage.js
@@ -2,15 +2,15 @@ var $nav = $('nav');
 
 // fullpage customization
 $('#fullpage').fullpage({
-  sectionsColor: ['#f4f4f4', '#f4f4f4', '#f4f4f4', '#f4f4f4', '#f4f4f4'],
+  
   sectionSelector: '.vertical-scrolling',
   slidesNavigation: true,
   controlArrows: false,
-  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  anchors: ['first', 'second', 'third', 'fourth', 'fifth'],
  
 
   onSlideLeave: function( anchorLink, index, slideIndex, direction) {
-    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+    if(anchorLink == 'fifth' && slideIndex == 1) {
       $.fn.fullpage.setAllowScrolling(true, 'up');
     }
   } 
